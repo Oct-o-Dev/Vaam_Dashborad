c022f778f2399e65b3dcb56c01156ffb69b84ad8
Oct-o-Dev <Abhineetk213@gmail.com>
2025-03-10 21:33:30 +0530

    try2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Static export
+  experimental: {
+    serverActions: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true }, // Required for static export
 };
 
 module.exports = nextConfig;
