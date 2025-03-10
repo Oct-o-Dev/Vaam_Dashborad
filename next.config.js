297d4bd59dc33dba6b43a184a2dd92cf937fbb69
Oct-o-Dev <Abhineetk213@gmail.com>
2025-03-10 21:05:27 +0530

    little changes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  experimental: {
-    serverActions: true, // Enable Server Actions
   },
   images: { unoptimized: true },
 };
